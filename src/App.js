645ba23221581772509cac97b59be794b2fcaabb
Rukshan Tharindu <97118309+Rukshan2000@users.noreply.github.com>
2024-06-09 10:14:56 +0530

    add page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,48 @@
-import React from 'react';
-import AutoPlayMusic from './pages/Page1.jsx';
+// import React from 'react';
+// import { BrowserRouter as Router, Route } from 'react-router-dom';
+// import HomePage from './pages/Home';
+// import Page1 from './pages/Page1';
 
-function App() {
+// function App() {
+//   return (
+//     <Router>
+//       <div className="App">
+//         <Route exact path="/" component={HomePage} />
+//         <Route path="/page2" component={Page1} />
+//         {/* Add more routes for additional pages */}
+//       </div>
+//     </Router>
+//   );
+// }
+
+// export default App;
+
+
+
+
+
+// App.jsx
+import React from 'react';
+import './App.css';
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
+import HomePage from './pages/Home';
+import Page1 from './pages/Page1';
+import Page2 from './pages/Page2';
+
+
+const App = () => {
   return (
-    <div className="App">
-      <AutoPlayMusic />
-    </div>
+    <BrowserRouter>
+      <Routes>
+
+        <Route path="/" element={<HomePage />} />
+        <Route path="/page1" element={<Page1 />} />
+        <Route path="/192837465" element={<Page2 />} />
+
+
+      </Routes>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
